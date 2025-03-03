c28a13b87f16c1bf11227b792eade486dc135a0e
Iconic <omkarfakeac123@gmail.com>
2025-03-03 14:38:04 +0530

    commit q7

--- 02_conditionals/12_Conditional_problem.py
+++ 02_conditionals/12_Conditional_problem.py
@@ -80,3 +80,24 @@
 #     print("Mode of transportation Car")
 # else:
 #     print("Enter proper mode")
+
+
+
+# Problem: Customize a coffee order: "Small", "Medium", or "Large" with an option for "Extra shot" of espresso.
+
+# coffeeOrder = {
+#     "coffeeSize":[
+#        "small" , "medium" ,"large" ],
+#     "extrashot":["true","false" ]  
+# }
+
+# coffeeSize = input("Enter the size of the coffee (small/medium/large)").lower()
+# coffeeShot = input("Enter do u want extra shot in coffee (true/false)").lower()
+
+# if coffeeSize in coffeeOrder["coffeeSize"]:
+#     if coffeeShot in coffeeOrder["extrashot"]:
+#         print(f"your customize is {coffeeSize} order and the extra shot is {coffeeShot} ")
+#     else:
+#         print("Enter proper coffee order")
+# else:
+#     print("coffee is not available")
